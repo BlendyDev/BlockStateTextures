98030585d47fa1bf8925362ca2b87390631e506b
GitHub Action <action@github.com>
2024-07-28 09:46:12 +0000

    Initialize template repository proper
    Set package name to com.github.blendydev.blockstatetextures
    Set project name to BlockStateTextures
    Set modid to blockstatetextures

--- src/main/java/com/github/blendydev/blockstatetextures/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/blendydev/blockstatetextures/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.blendydev.blockstatetextures.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
